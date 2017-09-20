396a222b40886024864de639ee392a29a946eea9
Boaz Rosenan <brosenan@gmail.com>
2017-09-20 15:16:31 +0300

    tl-1 -- inefficient

--- test/my_tweets/core_test.clj
+++ test/my_tweets/core_test.clj
@@ -4,15 +4,10 @@
             [cloudlog-events.testing :refer [scenario as emit query apply-rules]]))
 
 (fact
- ;; This test is written in the cloudlog testing DSL.
- ;; See http://axiom-clj.org/cloudlog-events.testing.html for more details.
  (scenario
-  (as "alice"
-      (emit [:my-tweets/task "alice" "Create app" 1000])
-      (emit [:my-tweets/task "alice" "Show app to @bob" 2000])
-      (query [:my-tweets/my-tasks "alice"]) => #{["alice" "Create app" 1000]
-                                              ["alice" "Show app to @bob" 2000]})
-  (apply-rules [:my-tweets.core/task-where-user-is-mentioned "bob"])
-  => #{["alice" "Show app to @bob" 2000]}
   (as "bob"
-      (query [:my-tweets/my-tasks "bob"]) => #{["alice" "Show app to @bob" 2000]})))
+      (emit [:my-tweets/tweet "bob" "hello" 1000]))
+  (as "alice"
+      (emit [:my-tweets/follow "alice" "bob"])
+      (query [:my-tweets/timeline "alice"])
+      => #{["bob" "hello" 1000]})))
