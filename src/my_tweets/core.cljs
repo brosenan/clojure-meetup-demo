a25927b288dabbe889d6396c0208473662f30a23
Boaz Rosenan <brosenan@gmail.com>
2017-09-20 14:02:14 +0300

    client-side

--- src/my_tweets/core.cljs
+++ src/my_tweets/core.cljs
@@ -7,69 +7,78 @@
 ;; Remove this before going to production...
 (enable-console-print!)
 
-;; A view allows the client to access (create, read, update and delete) facts of a certain form.
-;; See http://axiom-clj.org/axiom-cljs.macros.html#defview for more information.
-;; This view represents the :my-tweets/task fact.
-(defview task-view [user]
-  [:my-tweets/task user task ts]
-  ;; We want to store the view's state in a reagent atom so that changes to the view will be reflected in the DOM.
+(defview tweets-view [me]
+  [:my-tweets/tweet me text ts]
   :store-in (r/atom nil)
-  ;; We want the tasks to be sorted by timestamp, ascending.
-  :order-by ts)
+  :order-by (- ts))
 
-;; The following component edits a single task.
-;; It takes a single view entry and displays an edit box for the text, and a delete button to remove it.
-(defn edit-task [task-entry]
-  (let [{:keys [task ts swap! del!]} task-entry]
+(defn tweets-pane [host]
+  (let [tweets (tweets-view host (user host))
+        {:keys [add]} (meta tweets)]
     [:div
-     [:input {:value task
-              ;; When the field's value changes we use the provided swap! function to modify the view data.
-              :on-change #(swap! assoc :task (.-target.value %))}]
-     ;; To delete we use the provided del! function.
-     [:button {:on-click del!} "X"]]))
-
-;; The following component displays a list of all tasks, allowing them to be edited.
-;; It takes as parameter the host object, which represents the connection to the host.
-;; It provides a list of edit-task components, and a button for creating new tasks.
-(defn task-list [host]
-  ;; To get the list of items, we call the view function
-  (let [tasks (task-view host (user host))
-        {:keys [add]} (meta tasks)]
-    [:div
-     [:ul
-      (for [task-entry tasks]
-        [:li {:key (:ts task-entry)} ;; The timestamp is used as unique ID for this task
-         (edit-task task-entry)])]
-     ;; Create an empty task with the current time
      [:button {:on-click #(add {:ts ((:time host))
-                                :task ""})} "New Task"]]))
+                                :text ""})} "New Tweet"]
+     [:ul
+      (for [{:keys [text ts swap! del!]} tweets]
+        [:li {:key ts}
+         [:input {:value text
+                  :on-change #(swap! assoc :text (.-target.value %))}]
+         [:button {:on-click del!} "X"]])]]))
 
-;; A query allows the client to access results calculated by clauses.
-;; This query accesses :my-tweets/my-tasks.
-(defquery my-tasks-query [user]
-  [:my-tweets/my-tasks user -> author task ts]
+(defview following-view [me]
+  [:my-tweets/follow me other]
+  :store-in (r/atom nil))
+
+(defonce current-followee (r/atom ""))
+
+(defn following-pane [host]
+  (let [followees (following-view host (user host))
+        {:keys [add]} (meta followees)]
+    [:div
+     [:input {:value @current-followee
+              :on-change #(reset! current-followee (.-target.value %))}]
+     [:button {:on-click #(do
+                            (add {:other @current-followee})
+                            (reset! current-followee ""))} "Follow"]
+     [:ul
+      (for [{:keys [other del!]} followees]
+        [:li {:key other}
+         [:span other]
+         [:button {:on-click del!} "unfollow"]])]]))
+
+(defquery timeline-view [me]
+  [:my-tweets/timeline me -> author text ts]
   :store-in (r/atom nil)
-  :order-by ts)
+  :order-by (- ts))
 
-;; my-task-list displays all my tasks: those created by me, and those in which I am mentioned.
-(defn my-task-list [host]
-  (let [tasks (my-tasks-query host (user host))]
+(defn timeline-pane [host]
+  (let [timeline (timeline-view host (user host))]
     [:ul
-     (for [{:keys [author task ts]} tasks]
+     (for [{:keys [author text ts]} timeline]
        [:li {:key ts}
-        author ": " task])]))
+        author ": " text])]))
 
-;; 'host' is the object connecting this app to the host.
 (def host (ax/default-connection r/atom))
 
 (defn app []
   [:div
-   (task-list host)
-   (my-task-list host)])
+   [:h1 "Hello, " (user host)]
+   [:table
+    [:thead
+     [:tr
+      [:th "Timeline"]
+      [:th "Tweets"]
+      [:th "Following"]]]
+    [:tbody
+     [:tr
+      [:td
+       (timeline-pane host)]
+      [:td
+       (tweets-pane host)]
+      [:td
+       (following-pane host)]]]]])
 
-(defn render []
-  (let [elem (js/document.getElementById "app")]
-    (when elem
-      (r/render [app] elem))))
+(let [elem (js/document.getElementById "app")]
+  (when elem
+    (r/render [app] elem)))
 
-(render)
